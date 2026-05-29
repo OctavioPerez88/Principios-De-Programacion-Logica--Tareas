054b2b594fb10d6873d69b9ee95881b1730fedfe
Octavio 🦖 <octavioperez888@icloud.com>
2026-05-29 01:19:51 -0500

    Delete principios-de-programacion directory

--- principios-de-programacion/Ejercicio-2.js
+++
@@ -1,5 +0,0 @@
-// Ejercicio 2: Quiero que me imprima el numero de stock que tengo en mi inventario
-
-const numerodeproductos = 5
-const mensaje = "Número de productos en stock"
-console.log(mensaje + ": " + numerodeproductos)
